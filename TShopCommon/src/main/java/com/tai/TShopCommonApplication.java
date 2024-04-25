5759d51aec2c9cd96086fcaa27779302a9ce1cd0
tai <td.tai2000@gmail.com>
2024-04-25 10:38:56 +0700

    Initial commit

---
+++ TShopCommon/src/main/java/com/tai/TShopCommonApplication.java
@@ -0,0 +1,13 @@
+package com.tai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TShopCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TShopCommonApplication.class, args);
+    }
+
+}
---
+++ TShopWebParent/src/main/java/com/tai/TShopWebParentApplication.java
@@ -0,0 +1,13 @@
+package com.tai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TShopWebParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TShopWebParentApplication.class, args);
+    }
+
+}
